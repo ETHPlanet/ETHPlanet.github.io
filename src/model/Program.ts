8cc53d0dd4a53d5bca9e4eda689fc20ef99021c9
TechQuery <shiy007@qq.com>
2021-07-22 20:38:41 +0800

    [add] Program model & Scroll Bar utility for Speaker list

---
+++ src/model/Program.ts
@@ -0,0 +1,55 @@
+import {
+    BaseData,
+    MediaData,
+    NestedData,
+    CollectionModel,
+    Query,
+    service
+} from 'mobx-strapi';
+import { buildURLData } from 'web-utility/source/URL';
+
+import { User } from './User';
+import { Activity } from './Activity';
+
+export interface Program extends BaseData {
+    title: string;
+    start_time: string;
+    end_time: string;
+    summary?: string;
+    mentors: NestedData<User>[];
+    activity: NestedData<Activity>;
+    type: 'lecture' | 'workshop' | 'exhibition';
+    place?: NestedData<any>;
+    evaluations: any[];
+    accounts: any[];
+    documents: MediaData[];
+    verified: boolean;
+    category: NestedData<any>;
+    project?: NestedData<any>;
+    organization?: NestedData<any>;
+}
+
+export class ProgramModel extends CollectionModel<
+    Program,
+    'id' | 'title' | 'mentors'
+> {
+    name = 'program';
+    basePath = 'programs';
+
+    filter: Query<Program> = {} as Query<Program>;
+    page = 0;
+
+    async getList(filter = this.filter, page = this.page + 1, size = 12) {
+        const { body } = await service.get<Program[]>(
+            `${this.basePath}?${buildURLData({
+                ...filter,
+                _start: (page - 1) * size,
+                _limit: size
+            })}`
+        );
+        this.filter = filter;
+        this.page = page;
+        this.allItems.push(...body);
+        return body;
+    }
+}
--- src/model/index.ts
+++ src/model/index.ts
@@ -2,6 +2,8 @@ import { History } from 'cell-router/source';
 
 import './Base';
 import { ActivityModel } from './Activity';
+import { ProgramModel } from './Program';
 
 export const history = new History();
 export const activity = new ActivityModel();
+export const program = new ProgramModel();
